262e8c18bf50d219c957bd288bdd3350eb20ce2a
caspgin <31950557+caspgin@users.noreply.github.com>
2025-01-23 18:22:26 -0800

    Template for Page Transition

--- src/Inner/Inner.js
+++ src/Inner/Inner.js
@@ -11,66 +11,12 @@ export default function Inner({ children }) {
 		}
 	}
 
-	const opacity = {
-		initial: {
-			opacity: 0,
-		},
-		enter: {
-			opacity: 1,
-		},
-		exit: {
-			opacity: 1,
-		}
-	}
-
-	const slide = {
-		initial: {
-			top: "100vh",
-		},
-		enter: {
-			top: "100vh",
-		},
-		exit: {
-			top: 0,
-			transition: {
-				duration: 1,
-				ease: [0.76, 0, 0.24, 1]
-			}
-		}
-	}
-
-	const scale = {
-		initial: {
-			y: 0,
-			opacity: 1,
-			scale: 1
-		},
-		enter: {
-			y: 0,
-			opacity: 1,
-			scale: 1
-		},
-		exit: {
-			y: -100,
-			opacity: 0.5,
-			scale: 0.9,
-			transition: {
-				duration: 1.2,
-				ease: [0.76, 0, 0.20, 1]
-			}
-
-		}
-	}
-
 	return (
 		<motion.div className="inner">
-			<motion.div {...animate(slide)} className="slide" />
-			<motion.main {...animate(scale)} className="page">
-				<motion.div {...animate(opacity)} className="innerPage">
-					<HeaderSection />
-					{children}
-				</motion.div>
-			</motion.main>
+			<motion.div className="page">
+				<HeaderSection />
+				{children}
+			</motion.div>
 		</motion.div>
 	)
 }
